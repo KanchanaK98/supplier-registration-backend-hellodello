95a839aaf0a4ab3d3f671fcc0f620d75f5a7e145
kusham <kusham1998@gmail.com>
2023-01-22 06:05:06 +0530

    add check brand name route

--- routes/createSupplier.js
+++ routes/createSupplier.js
@@ -1,10 +1,12 @@
 const router = require("express").Router();
 
-const {createSupplier, checkEmail} = require("../controllers/createSupplier");
+const {createSupplier, checkEmail, checkBrandName} = require("../controllers/createSupplier");
 const {viewSupplier} = require("../controllers/viewSupplier");
 
 router.post("/add",createSupplier);
 router.post("/check/email/:email", checkEmail);
+router.post("/check/brandName/:brandName",checkBrandName);
+
 router.get("/view",viewSupplier);
 
 
